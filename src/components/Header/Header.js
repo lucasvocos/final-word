5bc52699ab9ce04662ea1dab2e5484aed5e75fa9
Lucas Vocos <lucas.vocos@me.com>
2019-11-21 12:24:43 -0500

    add footer and download functions

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Seal from '../../Assets/seal.svg';
+import Seal from '../../Assets/seal.png';
 import './Header.scss';
 
 const Header = () => {
   return (
     <header className='header'>
       <img src={Seal} alt='Presidential Seal' className='seal'/>
-      <h1>Aboard Air Force One</h1>
+      <h1 className='capitalized'>Aboard Air Force One</h1>
     </header>
   )
 }
