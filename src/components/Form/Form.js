25a455de0bf8781a65a2a8bc4d7115db524667f0
Lucas Vocos <lucas.vocos@me.com>
2019-11-21 10:26:18 -0500

    create form & add favicon

---
+++ src/components/Form/Form.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Form.scss';
+
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+  }
+
+  componentDidMount() {
+    this.textInput.current.focus();
+  }
+
+  render() {
+    return (
+      <form id='capture' className='tiny-hands-form' ref={this.textInput}>
+        <label htmlFor='notepad' className='hidden'>Note</label>
+        <textarea name='notepad' className='notepad' autoFocus placeholder='Quid pro quo'></textarea>
+      </form>
+    )
+  }
+}
